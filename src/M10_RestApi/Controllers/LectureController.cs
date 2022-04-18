7a92f0830dd54660855a363e7ab417b0165fa033
Max <yashanty@gmail.com>
2022-04-19 02:17:51 +0300

    cleaned LectureController code

--- src/M10_RestApi/Controllers/LectureController.cs
+++ src/M10_RestApi/Controllers/LectureController.cs
@@ -14,25 +14,15 @@ namespace M10_RestApi.Controllers
     [Route("/api/education/lecture")]
     public class LectureController : ControllerBase
     {
-        // private readonly IEntityService<ILecture> _entityService;
         private readonly IEntityServiceAsync<ILecture> _entityServiceAsync;
         private readonly IMapper _mapper;
 
-        public LectureController(IMapper mapper/*, IEntityService<ILecture> entityService*/, IEntityServiceAsync<ILecture> entityServiceAsync)
+        public LectureController(IMapper mapper, IEntityServiceAsync<ILecture> entityServiceAsync)
         {
             _mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
-
-            // _entityService = entityService ?? throw new System.ArgumentNullException(nameof(entityService));
             _entityServiceAsync = entityServiceAsync ?? throw new System.ArgumentNullException(nameof(entityServiceAsync));
         }
 
-        // [HttpGet("{id}")]
-        // public ActionResult<LectureDto> GetLecture(int id)
-        // {
-        //    var lecture = _entityService.GetEntity(id);
-        //    return lecture == null ? NotFound($"Lecture with Id = '{id}' not found.") : _mapper.Map<LectureDto>(lecture);
-        // }
-        //
         [HttpGet("{id}")]
         public async Task<ActionResult<LectureDto>> GetLectureAsync(int id)
         {
@@ -40,13 +30,6 @@ namespace M10_RestApi.Controllers
             return lecture == null ? NotFound($"Lecture with Id = '{id}' not found.") : Ok(_mapper.Map<LectureDto>(lecture));
         }
 
-        // [HttpGet("lectures")]
-        // public ActionResult<IReadOnlyCollection<LectureDto>> GetLectures()
-        // {
-        //    var result = _entityService.GetAllEntities().Select(i => _mapper.Map<LectureDto>(i)).ToArray();
-        //    return result.Length == 0 ? NotFound($"Lectures not found.") : result;
-        // }
-        //
         [HttpGet("lectures")]
         public async Task<ActionResult<IEnumerable<LectureDto>>> GetLecturesAsync()
         {
@@ -57,13 +40,6 @@ namespace M10_RestApi.Controllers
                 : Ok(lectures.Select(i => _mapper.Map<LectureDto>(i)));
         }
 
-        // [HttpPost]
-        // public ActionResult CreateLecture(LecturePostDto newLecture)
-        // {
-        //    var lecture = _entityService.CreateEntity(_mapper.Map<Lecture>(newLecture));
-        //    return Ok($"/api/education/lecture/{lecture.Id}");
-        // }
-        //
         [HttpPost]
         public async Task<ActionResult> CreateLectureAsync(LecturePostDto newLecture)
         {
@@ -71,18 +47,6 @@ namespace M10_RestApi.Controllers
             return Created($"/api/education/lecture/{lecture.Id}", lecture);
         }
 
-        // [HttpPut("{id}")]
-        // public ActionResult EditLecture(int id, LectureDto lecture)
-        // {
-        //    if (id != lecture.Id)
-        //    {
-        //        return BadRequest($"Requested Id = {id}, but given Id = {lecture.Id}");
-        //    }
-        //
-        //    var newLecture = _entityService.EditEntity(_mapper.Map<Lecture>(lecture));
-        //    return Ok($"/api/education/lecture/{newLecture.Id}");
-        // }
-        //
         [HttpPut("{id}")]
         public async Task<ActionResult> EditLectureAsync(int id, LectureDto lecture)
         {
@@ -95,13 +59,6 @@ namespace M10_RestApi.Controllers
             return Ok($"/api/education/lecture/{newLecture.Id}");
         }
 
-        // [HttpDelete("{id}")]
-        // public ActionResult DeleteLecture(int id)
-        // {
-        //    _entityService.DeleteEntity(id);
-        //    return Ok();
-        // }
-        //
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteLectureAsync(int id)
         {
