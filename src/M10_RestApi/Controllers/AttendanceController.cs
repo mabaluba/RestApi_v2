628e5e17a8e5d94c85162f1bcd3c025f790e05ea
Max <yashanty@gmail.com>
2022-05-20 20:23:17 +0300

    removed sync code

--- src/M10_RestApi/Controllers/AttendanceController.cs
+++ src/M10_RestApi/Controllers/AttendanceController.cs
@@ -15,31 +15,17 @@ namespace M10_RestApi.Controllers
     [Route("/api/education/attendance")]
     public class AttendanceController : ControllerBase
     {
-        // private readonly IEntityService<IAttendance> _entityService;
         private readonly IEntityServiceAsync<IAttendance> _entityServiceAsync;
         private readonly IMapper _mapper;
 
         public AttendanceController(
             IMapper mapper,
-
-            // IEntityService<IAttendance> entityService,
             IEntityServiceAsync<IAttendance> entityServiceAsync)
         {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-
-            // _entityService = entityService ?? throw new ArgumentNullException(nameof(entityService));
             _entityServiceAsync = entityServiceAsync;
         }
 
-        // [HttpGet("{id}")]
-        // public ActionResult<AttendanceDto> GetAttendance(int id)
-        // {
-        //    var attendance = _entityService.GetEntity(id);
-        //    return attendance == null
-        //        ? NotFound($"Attendance with Id = '{id}' not found.")
-        //        : Ok(_mapper.Map<AttendanceDto>(attendance));
-        // }
-        //
         [HttpGet("{id}")]
         public async Task<ActionResult<AttendanceDto>> GetAttendanceAsync(int id)
         {
@@ -59,13 +45,6 @@ namespace M10_RestApi.Controllers
                 : Ok(collection.Select(i => _mapper.Map<AttendanceDto>(i)));
         }
 
-        // [HttpPost]
-        // public ActionResult CreateAttendance(AttendancePostDto newAttendance)
-        // {
-        //    var attendance = _entityService.CreateEntity(_mapper.Map<Attendance>(newAttendance));
-        //    return Created($"/api/education/attendance/{attendance.Id}", attendance);
-        // }
-        //
         [HttpPost]
         public async Task<ActionResult> CreateAttendanceAsync(AttendancePostDto newAttendance)
         {
@@ -73,18 +52,6 @@ namespace M10_RestApi.Controllers
             return Created($"/api/education/attendance/{attendance.Id}", attendance);
         }
 
-        // [HttpPut("{id}")]
-        // public ActionResult EditAttendance(int id, AttendanceDto attendance)
-        // {
-        //    if (id != attendance.Id)
-        //    {
-        //        return BadRequest($"Requested Id = {id}, but given Id = {attendance.Id}. Please, correct input data.");
-        //    }
-        //
-        //    var newAttendance = _entityService.EditEntity(_mapper.Map<Attendance>(attendance));
-        //    return Ok($"/api/education/attendance/{newAttendance.Id}");
-        // }
-        //
         [HttpPut("{id}")]
         public async Task<ActionResult> EditAttendanceAsync(int id, AttendanceDto attendance)
         {
@@ -97,13 +64,6 @@ namespace M10_RestApi.Controllers
             return Ok($"/api/education/attendance/{newAttendance.Id}");
         }
 
-        // [HttpDelete("{id}")]
-        // public ActionResult DeleteAttendance(int id)
-        // {
-        //     _entityService.DeleteEntity(id);
-        //     return NoContent();
-        // }
-        //
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteAttendanceAsync(int id)
         {
